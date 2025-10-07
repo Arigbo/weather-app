50bcb66e36477765910435fce042ca29b733ac66
Arigbo Jesse <105205668+Arigbo@users.noreply.github.com>
2025-10-07 12:39:03 +0100

    update on nav

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -6,17 +6,17 @@ import SearchBox from "./Searchbox";
 type Props = {};
 export default function NavBar({}: Props) {
   return (
-    <nav className="shadow-sm sticky top-0 left-0 z-50 bg-white">
-      <div className="h-80px w-full flex justify-between items-center max-w-7xl px-3 max-auto">
-        <p className="flex items-center justify-center gap-2">
-          <h2 className="text-gray-500 text-3xl">Weather</h2>
-          <MdSunny className="text-3xl mt-1 text-yellow-300" />
-        </p>
-        <section className="flex gap-2 items-center">
-          <IoMdLocate className="text-2xl text-gray-400 hover:opacity-80 cursor-pointer "/>
-          <IoLocation className="text-3xl" />
-          <p className="text-slate-900/80 text-sm">Nigeria</p>
-          <SearchBox/>
+    <nav className="nav">
+      <div className="nav-inner">
+        <div className="nav-inner-left">
+          <h2 className="logo">Weather</h2>
+          <MdSunny className="sun" />
+        </div>
+        <section className="nav-inner-right">
+          <IoMdLocate className="location"/>
+          <IoLocation className="pin" />
+          <p className="">Nigeria</p>
+          <SearchBox />
         </section>
       </div>
     </nav>
